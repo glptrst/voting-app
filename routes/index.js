a1026a82696730c6305fb964d8ff95a76175f43c
gp <giulio.pietroiusti@gmail.com>
2018-11-29 00:32:59 +0100

    Add routes for GET and POST signup.

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 // GET /
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   return res.render('index', { title: 'Home' });
 });
 
 // GET /signin
-router.get('/signin', function(req, res, next) {
+router.get('/signin', (req, res, next) => {
   return res.render('signin', { title: 'Sign In' });
 });
 
 // GET /signup
+router.get('/signup', (req, res, next) => {
+  return res.render('signup', { title: 'Sign Up' });
+});
 
+// POST /signup
+router.post('/signup', (req, res, next) => {
+    // todo
+  return res.send('post request received!');
+});
 
 module.exports = router;

7e264eedebaadcd7829fc98b7b1a4da83ed556b4
gp <giulio.pietroiusti@gmail.com>
2019-03-23 21:47:00 +0100

    Correct names.

--- config.js
+++ config.js
@@ -5,14 +5,14 @@ if (process.env.PRODUCTION !== 'true') {
 module.exports = {
     PORT: process.env.PORT || 3000,
     db: {
-	DBURI: process.env.DBURI || configVariables.db.DBURI,
+	DBURI: process.env.db.DBURI || configVariables.db.DBURI,
     },
     mail: {
-	sender: process.env.sender || configVariables.mail.sender,
-	host: process.env.host || configVariables.mail.host,
-	port: process.env.port || configVariables.mail.port,
-	secure: process.env.secure || configVariables.mail.secure,
-	user: process.env.user || configVariables.mail.user,
-	pass: process.env.pass || configVariables.mail.pass
+	sender: process.env.mail.sender || configVariables.mail.sender,
+	host: process.env.mail.host || configVariables.mail.host,
+	port: process.env.mail.port || configVariables.mail.port,
+	secure: process.env.mail.secure || configVariables.mail.secure,
+	user: process.env.mail.user || configVariables.mail.user,
+	pass: process.env.mail.pass || configVariables.mail.pass
     }
 };
